244ff430f73d624a35339b39b2bafa6ea189aa76
Naoki_Fujisawa <naoki.fujisawa1@gmail.com>
2016-10-24 01:33:54 +0900

    クソみたいな線形リスト2種(ポインタ,カーソル)の追加

---
+++ algorithm/Ex3/InsertAfterCursor.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Null -1
+#define SIZE 1024
+
+typedef int Index;
+/* Member定義 */
+typedef struct {
+	int no;
+	char name[20];
+} Member;
+
+/* Node定義 */
+typedef struct node {
+	Member data;
+	Index next;
+	Index Dnext;
+} Node;
+
+/* List定義 */
+typedef struct {
+	Node *n;
+	Index head;
+	Index max;
+	Index deleted;
+	Index crnt;
+} List;
+
+/* 出力関数 */
+void PrintList(const List *list){
+	Index ptr = list->head;
+	if (Null == ptr) {
+		printf("No Data.\n");
+	} else {
+		puts("  Data");
+		puts("|---|--------------------|");
+		puts("| No|        Name        |");
+		puts("|---|--------------------|");
+		while (ptr != Null) {
+			printf("|%3d|%20s|\n", list->n[ptr].data.no, list->n[ptr].data.name);
+			ptr = list->n[ptr].next;
+		}
+		puts("|---|--------------------|");
+	}
+}
+
+static Index getIndex(List *list){
+	if (list->deleted == Null) {
+		return ++(list->max);
+	} else {
+		Index rec = list->deleted;
+		list->deleted = list->n[rec].Dnext;
+		return rec;
+	}
+}
+
+static void setNode(Node *n, const Member *x, Index next){
+	n->data = *x;
+	n->next = next;
+}
+
+/* 先頭挿入関数 */
+void InsertFront (List *list, const Member *x) {
+	Index ptr = list->head;
+	list->head = list->crnt = getIndex(list);
+	setNode(&list->n[list->head], x, ptr);
+}
+
+/* 位置任意挿入関数 */
+void InsertAfter(List *list, Index idx, const Member *x){
+	list->crnt = getIndex(list);
+	setNode(&list->n[list->crnt], x, list->n[idx].next);
+	list->n[idx].next = list->crnt;
+}
+
+int main() {
+	List list;
+	char text[10];
+
+	list.n       = calloc(SIZE, sizeof(Node));
+	list.head    = Null;
+	list.crnt    = Null;
+	list.max     = Null;
+	list.deleted = Null;
+
+	for(;;){
+		printf("何をしますか。(データの表示 : print / データの追加 : add / 終了 : exit)\n : ");
+		scanf("%s", text);
+		if ('p' == text[0]) {
+			PrintList(&list);
+		} else if ('a' == text[0]) {
+			int n, i;
+			Member addMember;
+			Index ptr = list.head;
+			if (Null == ptr){
+				printf("no   : "); scanf("%d", &addMember.no);
+				printf("name : "); scanf("%s", addMember.name);
+				InsertFront(&list, &addMember);
+			} else {
+				printf("何個目の次に挿入しますか\n: "); scanf("%d", &n);
+				if (0 == n) {
+					printf("No   : "); scanf("%d", &addMember.no);
+					printf("name : "); scanf("%s", addMember.name);
+					InsertFront(&list, &addMember);
+				} else {
+					for(i = 1; i < n; i++){
+						if (Null == list.n[ptr].next) {
+							printf("指定値が大きすぎです。\n最後に追加します。\n");
+							break;
+						}
+						ptr = list.n[ptr].next;
+					}
+					printf("No   : "); scanf("%d", &addMember.no);
+					printf("name : "); scanf("%s", addMember.name);
+					InsertAfter(&list, ptr, &addMember);
+				}
+			}
+		} else if ('e' == text[0]){
+			printf("終了します\n");
+			/* メモリ解放 */
+			while (Null == list.head){
+				Index ptr = list.n[list.head].next;
+				Index del = list.deleted;
+				list.deleted = list.head;
+				list.n[list.head].Dnext = del;
+				list.head = ptr;
+			}
+			free(list.n);
+			break;
+		}
+	}
+	//InsertAfter(&list, AllocNode(),)
+	return 0;
+}
---
+++ algorithm/Ex3/InsertAfterPoint.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Member定義 */
+typedef struct {
+	int no;
+	char name[20];
+} Member;
+
+/* Node定義 */
+typedef struct node {
+	Member data;
+	struct node *next;
+} Node;
+
+/* List定義 */
+typedef struct {
+	Node *head;
+	Node *crnt;
+} List;
+
+/* 出力関数 */
+void PrintList(const List *list){
+	Node *ptr = list->head;
+	if (NULL == ptr) {
+		printf("No Data.\n");
+	} else {
+		puts("  Data");
+		puts("|---|--------------------|");
+		puts("| No|        Name        |");
+		puts("|---|--------------------|");
+		while (ptr != NULL) {
+			printf("|%3d|%20s|\n", ptr->data.no, ptr->data.name);
+			ptr = ptr->next;
+		}
+		puts("|---|--------------------|");
+	}
+}
+
+/* Node領域確保 */
+Node *AllocNode(void){
+	return calloc(1, sizeof(Node));
+}
+
+/* 先頭挿入関数 */
+void InsertFront (List *list, const Member *x) {
+	Node *ptr = list->head;
+	list->crnt = AllocNode();
+	list->head = list->crnt;
+	list->crnt->data = *x;
+	list->crnt->next = ptr;
+	list->crnt =NULL;
+}
+
+/* 位置任意挿入関数 */
+void InsertAfter(List *list, Node *p, const Member *x){
+	list->crnt = AllocNode();
+	list->crnt->data = *x;
+	list->crnt->next = p->next;
+	p->next = list->crnt;
+	list->crnt = NULL;
+}
+
+int main() {
+	List list;
+	char text[10];
+
+	list.head = NULL;
+	list.crnt = NULL;
+
+	for(;;){
+		printf("何をしますか。(データの表示 : print / データの追加 : add / 終了 : exit)\n:");
+		scanf("%s", text);
+		if ('p' == text[0]) {
+			PrintList(&list);
+		} else if ('a' == text[0]) {
+			int n, i;
+			Member addMember;
+			Node *ptr = list.head;
+			if (NULL == ptr){
+				printf("no   : "); scanf("%d", &addMember.no);
+				printf("name : "); scanf("%s", addMember.name);
+				InsertFront(&list, &addMember);
+			} else {
+				printf("何個目の次に挿入しますか\n: "); scanf("%d", &n);
+				if (0 == n) {
+					printf("No   : "); scanf("%d", &addMember.no);
+					printf("name : "); scanf("%s", addMember.name);
+					InsertFront(&list, &addMember);
+				} else {
+					for(i = 1; i < n; i++){
+						if (NULL == ptr->next) {
+							printf("指定値が大きすぎです。\n最後に追加します。\n");
+							break;
+						}
+						ptr = ptr->next;
+					}
+					printf("No   : "); scanf("%d", &addMember.no);
+					printf("name : "); scanf("%s", addMember.name);
+					InsertAfter(&list, ptr, &addMember);
+				}
+			}
+		} else if ('e' == text[0]){
+			printf("終了します\n");
+			/* メモリ解放 */
+			while (NULL != list.head){
+				Node *ptr = list.head->next;
+				free(list.head);
+				list.head = ptr;
+			}
+			break;
+		}
+	}
+	return 0;
+}
