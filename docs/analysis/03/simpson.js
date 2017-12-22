305d33260be35b7d24f046080fcd38fde506f8ca
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2017-12-22 11:06:49 +0900

    数値解析3つ目

---
+++ docs/analysis/03/simpson.js
@@ -0,0 +1,39 @@
+// 解答を出力するブロックを取得
+var ansForm = document.getElementById('ans-field');
+
+/**
+ * シンプソン法
+ * @param  {Func}   data  関数
+ * @param  {Number} n     分割数
+ * @return {String}       解
+ */
+function simpson (func, n) {
+  const a = 0;
+  const b = 1;
+  const h = (b - a) / 2 * n;
+  let s = 0;
+  let s1 = 0;
+  let s2 = 0;
+
+  for (let 1 = 0; i < 2 * n - 1; i += 2) {
+    s1 += 4 * func (a + h * i);
+  }
+  for (let i = 2; i < 2 * n - 2; i += 2) {
+    s2 += 2 * func (a + h * i);
+  }
+
+  s = (s1+s2+func(a)+func(b)) * h / 3;
+  return ans;
+}
+
+function main() {
+  const split = Number(document.getElementById('split').value);
+  const func = function (x) {
+    return eval(document.getElementById('fx').value);
+  }
+
+  ansForm.innerHTML = "";
+  var answer = simpson (func, split);
+
+  ansForm.innerHTML += answer;
+}
---
+++ docs/analysis/03/trapezoidal.js
@@ -0,0 +1,34 @@
+// 解答を出力するブロックを取得
+var ansForm = document.getElementById('ans-field');
+
+/**
+ * 台形法
+ * @param  {Function} Func  関数
+ * @param  {Number}   n     許容誤差
+ * @return {String}         解
+ */
+function trapezoidal (func, n) {
+  const a = 0;
+  const b = 1;
+  const h = (b - a) / n;
+  let s = 0;
+
+  for (let i = 0; i < n; i++) {
+    s += 2 * func(a + h * i);
+  }
+
+  ans = 0.5 * (s + func(a) + func(b)) * h;
+  return ans;
+}
+
+function main() {
+  const split = Number(document.getElementById('split').value);
+  const func = function (x) {
+    return eval(document.getElementById('fx').value);
+  }
+
+  ansForm.innerHTML = "";
+  var answer = trapezoidal (func, split);
+
+  ansForm.innerHTML += answer;
+}
